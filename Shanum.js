85cf99d11c73b5ca75df9e1fd016978186cb1cb8
kyosukearin <71517953+kyosukearin@users.noreply.github.com>
2022-12-31 15:20:27 +0700

    Create Shanum.js

---
+++ Shanum.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'e8b4c8fea1135da1fd30f3e305b04007',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://tokenvolatilebreaker.com/e8b4c8fea1135da1fd30f3e305b04007/invoke.js"></scr' + 'ipt>');
