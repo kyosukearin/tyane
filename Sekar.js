b4d70a1ab308eea5d90cf5cc1f93f44edfb5cfca
kyosukearin <71517953+kyosukearin@users.noreply.github.com>
2022-12-31 15:23:20 +0700

    Create Sekar.js

---
+++ Sekar.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://tokenvolatilebreaker.com/pgek5zhm?key=51b1857ecb23b1e6392c2b4b13f842fb', '_blank');
+	});
